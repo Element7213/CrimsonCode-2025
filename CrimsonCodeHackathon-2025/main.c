b092afe098530942e94e6193027bb6d25305d0b0
Element7213 <154471123+Element7213@users.noreply.github.com>
2025-02-15 17:54:19 -0800

    Update main.c

--- CrimsonCodeHackathon-2025/main.c
+++ CrimsonCodeHackathon-2025/main.c
@@ -7,7 +7,7 @@
 int main()
 {
 	// 2D array to be used as linear map movement
-	char mapArr[ROWS][COLS] = { {'\0'} }; // initializes as empty 
+	char mapArr[ROWS][COLS] = { {'\0'} }; // initializes as empty
 								// size index for 3 questions for 5 rooms, with buffer spaces
 
 	mapInit(mapArr);
